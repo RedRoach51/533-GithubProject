569d393107210ab156222c8d886b850572c55911
RedRoach51 <RedRoach51@gmail.com>
2021-03-29 21:39:53 -0400

    Modified ProjectReader to retrieve & calculate statistics for contributors from a given repo.

--- ProjectReader.py
+++ ProjectReader.py
@@ -14,12 +14,12 @@ from dateutil.relativedelta import relativedelta
 token = os.getenv('GITHUBTOKEN')
 
 
-def getGithubCommits(repo = "SSW-567-HW04", user = "redroach51"):
-    commits_orig = requests.get("https://api.github.com/repos/" + user + "/" + repo + 
-                                    "/commits?simple=yes&per_page=100&page=1",
+def getGithubCommits(repo = "533-GithubProject", user = "redroach51"):
+    commits_orig = requests.get('https://api.github.com/repos/{user}/{repo}/commits?simple=yes&per_page=100&page=1'
+                                    .format(user=user, repo=repo),
                                 headers={"Authorization": token})
     commits = commits_orig.json();
-    total_commits = len(commits)
+
     
     while 'next' in commits_orig.links.keys():
         commits_orig = requests.get(commits_orig.links['next']['url'],
@@ -28,22 +28,67 @@ def getGithubCommits(repo = "SSW-567-HW04", user = "redroach51"):
  
     total_commits = len(commits)
     
-    output(commits);
+#   examineJSON(commits);
     return len(commits);
 
-def output(json_object):
-   with open('TempOutput.txt','w') as outfile:
-       json.dump(json_object,outfile)
+def getGithubContributors(repo = "533-GithubProject", user = "redroach51"):
+    
+    contrib_orig = requests.get('https://api.github.com/repos/{user}/{repo}/contributors?simple=yes&per_page=100&page=1'
+                                    .format(user=user, repo=repo),
+                                headers={"Authorization": token})
+    contributors = contrib_orig.json();
+    while 'next' in contrib_orig.links.keys():
+        contrib_orig = requests.get(contrib_orig.links['next']['url'],
+                        headers = {"Authorization": token});
+        contributors.extend(contrib_orig.json())
+    
+    contrib_list = []
+    for contributor in contributors:
+        contrib_list.append([contributor["login"],contributor["contributions"]])
+
+#   examineJSON(contributors)    
+    return contrib_list        
+
+def getContributorStatistics(contributors):
+    contrib_list = []
+    lurker_count = 0
+    
+    for contributor in contributors:
+        contrib_list.append(contributor[1])
+    
+    top_contrib = max(contrib_list)
+    for num in contrib_list:
+        if num < top_contrib / 10:
+            lurker_count += 1
+    
+    return [len(contrib_list),lurker_count,max(contrib_list)]
+
+def examineJSON(json_object):
+   with open('TempOutput.txt','w') as file:
+       json.dump(json_object,file)
 
 def main():
     user_name = input("Input GitHub repo's owner: ")
     user_repo = input("Input GitHub repo's name: ")
     if (user_name == "" or user_repo == ""):
-        print("Default test API set to creator RedRoach51's host repo.")
+        print("Default test API set to creator RedRoach51's host repo.\n")
         user_name = "redroach51"
-        user_repo = "SSW-567-HW04"
+        user_repo = "533-GithubProject"
     commits = getGithubCommits(user_repo,user_name)
-    print(user_repo + " Commits: " + str(commits))
+    
+    print("\n" + user_repo + " Commits: " + str(commits) + "\n")
+    
+
+    contributors = getGithubContributors(user_repo,user_name)
+    contributor_stats = getContributorStatistics(contributors)
+
+    print("Contributors ({total_contributors}):"
+          .format(total_contributors=contributor_stats[0]))    
+#debug    for contributor in contributors:
+#        print('{contributor}: {contributions}'
+#                  .format(contributor=contributor[0], contributions=contributor[1]))
+    print("Total Lurkers (< 10% of {max_contribution}) : {lurker_count}"
+              .format(max_contribution = contributor_stats[2], lurker_count = contributor_stats[1]))
 
 if __name__ == '__main__':
     main()
